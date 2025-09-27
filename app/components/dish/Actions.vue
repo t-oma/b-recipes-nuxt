021e7c5696743fc10d7d49924bad58cf1e7363f8
Artem Levchenko <levchencode@gmail.com>
2025-09-27 21:34:45 +0300

    actions

---
+++ app/components/dish/Actions.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="flex gap-2 py-4">
+    <button
+      type="button"
+      class="border-border inline-flex items-center justify-center gap-2 rounded-md border px-4 py-2"
+    >
+      <Icon
+        name="lucide:printer"
+        class=""
+        size="16"
+      />
+      <span>PDF</span>
+    </button>
+    <button
+      type="button"
+      class="border-border inline-flex items-center justify-center gap-2 rounded-md border px-4 py-2"
+    >
+      <Icon
+        name="lucide:clipboard-list"
+        class=""
+        size="16"
+      />
+      <span>Інструкція</span>
+    </button>
+  </div>
+</template>
